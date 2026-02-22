c269b17de54cebce3e1de938e2409c81e1f619fa
jsh <408cake@gmail.com>
2026-02-23 05:44:06 +0900

    반복문학습

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/stmt_condition')
-def get_stmt_condition():
-    user_role = 'member'  # Example: 'admin', 'member', 'guest'
-    return render_template('stmt_condition.html', role=user_role)
+@app.route('/stmt_iteration')
+def get_stmt_iteration():
+    fruits = ['Apple', 'Banana', 'Cherry', 'Melon', 'Lemon']
+    return render_template('stmt_iteration.html', l=fruits)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=31337)
